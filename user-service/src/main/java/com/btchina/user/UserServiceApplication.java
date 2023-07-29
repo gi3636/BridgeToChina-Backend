dee00293fb8ef0755bc1950c350e8cee5003ef4c
gi3636 <fenggi123@gmail.com>
2023-07-29 20:41:15 +0800

    feat:添加调试信息

--- user-service/src/main/java/com/btchina/user/UserServiceApplication.java
+++ user-service/src/main/java/com/btchina/user/UserServiceApplication.java
@@ -8,9 +8,11 @@ import com.btchina.core.config.EnableLongToStringConfig;
 import com.btchina.core.exception.EnableDefaultExceptionAdvice;
 import com.btchina.feign.config.DefaultFeignConfiguration;
 import com.btchina.redis.config.EnableRedisSerialize;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @EnableMybatisPlus
 @EnableSwagger
@@ -22,6 +24,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableJwt
 @EnableMq
 @EnableFeignClients(basePackages = {"com.btchina.feign.clients"}, defaultConfiguration = DefaultFeignConfiguration.class)
+@Slf4j
 public class UserServiceApplication {
 
     public static void main(String[] args) {
--- user-service/src/test/java/com/btchina/user/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/btchina/user/UserServiceApplicationTests.java
@@ -1,13 +1,19 @@
 package com.btchina.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        //获取当前配置yml文件的环境变量
+        ConfigurableEnvironment env = SpringApplication.run(UserServiceApplication.class).getEnvironment();
+        System.out.println("获取当前配置yml文件的环境变量: " + env.getProperty("spring.datasource.url"));
     }
+
 
 }
